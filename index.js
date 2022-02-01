d6a4db0a8de687d3d3640206f3aeefffadfb8b3b
zt-deep <84309703+zt-deep@users.noreply.github.com>
2022-02-01 23:51:47 +0530

    Updated entry file

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const FILE_NAME = 'Index.js';
+const bodyParser = require('body-parser');
 const compression = require('compression');
+require('dotenv').config();
 const express = require('express');
 const logger = require('./logger/index')(FILE_NAME);
+const { errorResponse } = require('./lib/common/Util')
+
+require('./database/models/mainDb/associations');
 require('./database/index');
+
 
 const app = express();
 app.use(compression());
+app.use(bodyParser.json());
+
+app.use('/ats/',require('./src/routes/index'));
 
 app.get('/', async (req, res) => {
     res
@@ -14,7 +23,7 @@ app.get('/', async (req, res) => {
         message: 'Backend Is working'
       });
 })
-
+app.use(errorResponse);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     logger.info(`Zimyo ATS is live on ${PORT}`);

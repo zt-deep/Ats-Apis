1842ca208cc42f73e34affc9a0dd8ff0d1746189
zt-deep <84309703+zt-deep@users.noreply.github.com>
2022-02-03 01:00:56 +0530

    added authentication routes

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { HttpCodes, SUCCESS_MESSAGE } = require('../../config/Constants');
 const { formatResponse } = require('../../lib/common/Util');
 const userLogin = require('../controllers/AuthController/Login');
-const { createSession } = require('../controllers/AuthController/Session');
+const { createSession, validateToken } = require('../controllers/AuthController/Session');
 
 const app = express();
 
 // User Login
-app.use('/login', (req, res, next) => {
+app.post('/login', (req, res, next) => {
     userLogin(req.body, req.auth, (err, data) => {
         if(err)
             next(err);
@@ -19,8 +19,19 @@ app.use('/login', (req, res, next) => {
 });
 
 // Create Session and generate jwt token
-app.use('/token', (req, res, next) => {
+app.post('/token', (req, res, next) => {
   createSession(req, (err, data) => {
+    if (err) next(err);
+    else {
+      const response = formatResponse(HttpCodes.OK, SUCCESS_MESSAGE, data);
+      res.status(HttpCodes.OK).json(response);
+    }
+  });
+});
+
+// Create Session and generate jwt token
+app.post('/validate', (req, res, next) => {
+  validateToken(req, (err, data) => {
     if (err) next(err);
     else {
       const response = formatResponse(HttpCodes.OK, SUCCESS_MESSAGE, data);

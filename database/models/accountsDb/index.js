69870b34ad37c841d127b339ddb6a9da2d1dc9ef
zt-deep <84309703+zt-deep@users.noreply.github.com>
2022-02-03 01:00:23 +0530

    included new tables in accounts db index file

--- database/models/accountsDb/index.js
+++ database/models/accountsDb/index.js
@@ -3,23 +3,20 @@ const accountsDbSequilizeInstance = require('../../connection/accountsDb');
 /**
  * Syncing ACCOUNTS database tables with sequelize
  */
-const AppMenuMasterModel =
-  require('./tables/AppMenuMaster.model')(
-    accountsDbSequilizeInstance
-  );
-const OrgAppMenuMappingModel =
-  require('./tables/OrgAppMenuMapping.model')(
-    accountsDbSequilizeInstance);
-const OrgUserAppMenuMapping =
-  require('./tables/OrgUserAppMenuMapping.model')(
-    accountsDbSequilizeInstance
-  );
-const OrgRoleMenuMapping =
-  require('./tables/OrgRoleMenuMapping.model')(
-    accountsDbSequilizeInstance
-  );
+const AppMenuMasterModel = require('./tables/AppMenuMaster.model')(
+  accountsDbSequilizeInstance
+);
+const OrgAppMenuMappingModel = require('./tables/OrgAppMenuMapping.model')(
+  accountsDbSequilizeInstance
+);
+const OrgUserAppMenuMapping = require('./tables/OrgUserAppMenuMapping.model')(
+  accountsDbSequilizeInstance
+);
+const OrgRoleMenuMapping = require('./tables/OrgRoleMenuMapping.model')(
+  accountsDbSequilizeInstance
+);
 const SessionsModel = require('./tables/Sessions.model')(
-    accountsDbSequilizeInstance
+  accountsDbSequilizeInstance
 );
 const SessionsHistoriesModel = require('./tables/SessionHistories.model')(
   accountsDbSequilizeInstance

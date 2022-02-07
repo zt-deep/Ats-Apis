bdac64d2a8f30ee97fc7a019da6f6d8769d45996
zt-deep <84309703+zt-deep@users.noreply.github.com>
2022-02-07 22:29:19 +0530

    added menu routes

--- src/routes/MenuRoutes.js
+++ src/routes/MenuRoutes.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const MenuController = require('../controllers/MenuController');
+const { HttpCodes, SUCCESS_MESSAGE } = require('../../config/Constants');
+const { formatResponse } = require('../../lib/common/Util');
+const getUserMenuAndActivities = require('../controllers/MenuController/UserMenuAndActivities');
 
 const app = express();
 
+/**
+ * Fetch Menus and Activities For Logged In User
+ */
 app.get('/getMenus', (req, res, next) => {
-    MenuController.getMenusAndActivities(req.params, req.auth, (error, data) => {
-        if (error) next();
-        else 
+    getUserMenuAndActivities(req.body, req.auth, (error, data) => {
+        if (error) 
+            next(error);
+        else{
+            const response = formatResponse(
+              HttpCodes.OK,
+              SUCCESS_MESSAGE,
+              data
+            );
+            res.status(HttpCodes.OK).json(response);
+        }
     });
 })
+
+module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const verify = require('../common/middleware');
 const authRoutes = require('./AuthRoutes');
+const menuRoutes = require('./MenuRoutes');
 
 const app = express();
 
-app.use('/auth', authRoutes);
+app.use('/auth', verify ,authRoutes);
+app.use('/menu', verify, menuRoutes);
 
 module.exports = app;
